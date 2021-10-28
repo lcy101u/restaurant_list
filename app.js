1214ddf49382385182b0f5b44718be2913b2ceb4
Young <phanton0117@gmail.com>
2021-10-29 05:02:03 +0800

    [ADD] Add handlebars and controller

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', )
+//setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+//setting static files
+app.use(express.static('public')) //告訴express靜態檔案是放在名為 public 的資料夾中
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results})
+})
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() ===req.params.restaurant_id)
+  res.render('show', {restaurant})
+})
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase().trim()
+  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword) || restaurant.name_en.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword))
+  console.log(restaurants, keyword)
+  res.render('index', {restaurants, keyword})
+})
+//start and listening on the Express server
+app.listen(port , () => {
+  console.log(`Express is listening on port ${port}`)
+})
