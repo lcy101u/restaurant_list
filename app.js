01e16cba64f7804ba104c4ba18ebe7b67a9ac429
Young <phanton0117@gmail.com>
2021-12-13 22:14:08 +0800

    fix: Optimize app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./modules')
 const app = express()
 const port = 3000
-
-require('./config/mongoose')
 
 //setting template engine
 app.engine('handlebars', exphbs({ 
@@ -25,19 +22,6 @@ app.use(express.static('public')) //告訴express靜態檔案是放在名為 pub
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase().trim()
-  Restaurant.find()
-    .lean()
-    .then(restaurants => {
-      const filteredRestaurantsData = restaurants.filter(
-        restaurant => restaurant.name.toLowerCase().includes(keyword) || 
-                      restaurant.name_en.toLowerCase().includes(keyword) || 
-                      restaurant.category.toLowerCase().includes(keyword))
-      res.render('index', {restaurants: filteredRestaurantsData, keyword})
-    })
-})
 
 //start and listening on the Express server
 app.listen(port , () => {
