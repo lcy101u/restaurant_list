0907bde62f3ab3bd16cf2135e5a367877ff026f6
Young <phanton0117@gmail.com>
2022-02-01 04:15:34 +0800

    feat: add relations between users and restaurants models

--- modules/routes/restaurants.js
+++ modules/routes/restaurants.js
@@ -4,33 +4,38 @@ const Restaurant = require('../../models/restaurant')
 
 router.get('/new', (req, res) => res.render('new'))
 router.post('/', (req, res) => {
+  req.body.userId = req.user._id
   return Restaurant.create(req.body)
     .then(() => res.redirect('/'))
     .catch(err => console.error(err))
 })
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurant) => res.render('show', {restaurant}))
     .catch(error => console.error(error))
 })
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurant) => res.render('edit', {restaurant}))
     .catch(error => console.error(error))
 })
 router.put('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findByIdAndUpdate(id, req.body)
+  req.body.userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOneAndUpdate(_id, req.body)
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.error(error))
 })
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(err => console.error(err))
