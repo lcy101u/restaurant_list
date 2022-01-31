b703bc426badb50a36c5e00eb17fd8103ce44f36
Young <phanton0117@gmail.com>
2022-01-31 15:46:06 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./modules')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -18,7 +19,11 @@ app.engine('handlebars', exphbs({
   }
 }))
 app.set('view engine', 'handlebars')
-
+app.use(session({
+  secret: 'RestaurantSecret',
+  resave: false,
+  saveUninitialized: true
+}))
 //setting static files
 app.use(express.static('public')) //告訴express靜態檔案是放在名為 public 的資料夾中
 app.use(bodyParser.urlencoded({extended: true}))
