6e075313afbe6dfa0ee336e19a0dcf7617acda05
Young <phanton0117@gmail.com>
2022-01-31 23:14:51 +0800

    feat: add user authentication in home and restaurant page

--- modules/index.js
+++ modules/index.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const home = require('./routes/home')
 const users = require('./routes/users')
 const restaurants = require('./routes/restaurants')
-router.use('/', home)
-router.use('/restaurants', restaurants)
+const { authenticator } = require('../middleware/auth')
+
+router.use('/restaurants',authenticator, restaurants)
 router.use('/users', users)
+router.use('/', authenticator, home)
+
 module.exports = router
--- modules/routes/users.js
+++ modules/routes/users.js
@@ -2,33 +2,44 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const User = require('../../models/user')
-router.get('/login', (req, res) => {
-  res.render('login')
-})
-router.get('/register', (req, res) => {
-  res.render('register')
-})
+
+//login
+router
+  .route('/login')
+  .get((req, res) => {
+    res.render('login')
+  })
+  .post(passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  }))
+
+//register
+router
+  .route('/register')
+  .get((req, res) => {
+    res.render('register')
+  })
+  .post((req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    console.table(req.body)
+    User.findOne({ email })
+      .then(user => {
+        if(user) {
+          console.log('User already exists!')
+          res.render('register', { name, email, password, confirmPassword })
+        } else {
+          return User.create({name, email, password})
+            .then(() => res.redirect('/'))
+            .catch(err => console.err(err))
+        }
+      })
+  })
+
+//logout
 router.get('/logout', (req, res) => {
-  req.logout()
+  req.logout() //passport 提供的function，會清除session
   res.redirect('/users/login')
 })
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-router.post('/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  console.table(req.body)
-  User.findOne({ email })
-    .then(user => {
-      if(user) {
-        console.log('User already exists!')
-        res.render('register', { name, email, password, confirmPassword })
-      } else {
-        return User.create({name, email, password})
-          .then(() => res.redirect('/'))
-          .catch(err => console.err(err))
-      }
-    })
-})
+
 module.exports = router
