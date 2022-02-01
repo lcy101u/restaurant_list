572f24e34f6c8593041276203224109daba0045e
Young <phanton0117@gmail.com>
2022-02-01 09:53:54 +0800

    refactor: router refactor

--- modules/routes/restaurants.js
+++ modules/routes/restaurants.js
@@ -9,14 +9,33 @@ router.post('/', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(err => console.error(err))
 })
-router.get('/:id', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  return Restaurant.findOne({ _id, userId })
-    .lean()
-    .then((restaurant) => res.render('show', {restaurant}))
-    .catch(error => console.error(error))
-})
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const userId = req.user._id
+    const _id = req.params.id
+    return Restaurant.findOne({ _id, userId })
+      .lean()
+      .then((restaurant) => res.render('show', {restaurant}))
+      .catch(error => console.error(error))
+  })
+  .put((req, res) => {
+    req.body.userId = req.user._id
+    const _id = req.params.id
+    return Restaurant.findOneAndUpdate(_id, req.body)
+      .then(() => res.redirect(`/restaurants/${id}`))
+      .catch(error => console.error(error))
+  })
+  .delete((req, res) => {
+    const userId = req.user._id
+    const _id = req.params.id
+    return Restaurant.findOne({ _id, userId })
+      .then(restaurant => restaurant.remove())
+      .then(() => res.redirect('/'))
+      .catch(err => console.error(err))
+  })
+
 router.get('/:id/edit', (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
@@ -24,21 +43,6 @@ router.get('/:id/edit', (req, res) => {
     .lean()
     .then((restaurant) => res.render('edit', {restaurant}))
     .catch(error => console.error(error))
-})
-router.put('/:id', (req, res) => {
-  req.body.userId = req.user._id
-  const _id = req.params.id
-  return Restaurant.findOneAndUpdate(_id, req.body)
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.error(error))
-})
-router.delete('/:id', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  return Restaurant.findOne({ _id, userId })
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(err => console.error(err))
 })
 
 module.exports = router
